d9f4a1b95c6eb7988775e3f292886854c7ab731e
Marek Belis <marek.belis810@gmail.com>
2023-09-15 20:24:01 +0200

    added links fei,linkedin

--- MyPortfolio/src/components/AboutMe.jsx
+++ MyPortfolio/src/components/AboutMe.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import Card from "./Card"
 import "../styles/aboutMe.css"
 import GitLogo from "../assets/git.png"
+import LILogo from "../assets/Linkedin-Logo.png"
+import Fei from "../assets/fei.png"
 
 
 export default function AboutMe(){
@@ -10,6 +12,8 @@ export default function AboutMe(){
        <Card />
        <div className="liks--wrapper">
         <a  target="_blank" href="https://github.com/MarekBs"><img className="git--logo" src={GitLogo} alt="GitHub logo" /></a>
+        <a  target="_blank" href="https://www.linkedin.com/in/marek-belis-a11591231/"><img className="git--logo" src={LILogo} alt="LinkedIn logo" /></a>
+        <a  target="_blank" href="https://www.fei.stuba.sk/"><img className="git--logo" src={Fei} alt="FEI-STU logo" /></a>
         
        </div>
        </div>
