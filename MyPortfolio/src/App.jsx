6c84f9a1f569411d50fe260d30c2dcb3b337bdc0
Marek Belis <marek.belis810@gmail.com>
2023-09-17 16:12:29 +0200

    changed nav

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Header from "./components/Header"
 import AboutMe from "./components/AboutMe"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 function App() {
--- MyPortfolio/src/components/Header.jsx
+++ MyPortfolio/src/components/Header.jsx
@@ -16,11 +16,31 @@ export default function Header(){
     }
  
     return(
-        <header className="header--container">
-            <h1>about ME</h1>
-            <div className="sun--wrappper" onClick={changeMode}>
+        <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+        about ME
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="sun--wrappper" onClick={changeMode}>
             <img className={`sun ${mode ? "sun-rotaterev" : "sun-rotate"}`}  height={20} src={mode ?sunLight : sunDark} alt="sun" />
             </div>
-        </header>
+        </div>
+      </div>
+    </nav>
+    </header>
     )
 }
