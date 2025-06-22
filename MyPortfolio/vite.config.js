2705cf30ce6020dad9707e402f4948918bbb16f6
Marek Belis <marek.belis810@gmail.com>
2025-06-22 14:17:26 +0200

    fix: add base to vite config 2

--- MyPortfolio/vite.config.js
+++ MyPortfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/aboutMe/MyPortfolio', // <- názov tvojho repo
+  base: '/aboutMe/MyPortfolio/', // <- názov tvojho repo
   plugins: [react()],
 })
